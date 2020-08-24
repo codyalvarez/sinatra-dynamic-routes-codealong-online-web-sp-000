67e8f1e5dedb30c4de45436bc3c01864134975a1
Cody Alvarez <cody_alvarez19@outlook.com>
2020-08-24 18:27:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,12 +14,9 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
-  get '/goodbye/:name' do
-    # @user_name = user_name.select do |name|
-      @user_name == params[:name]
-      "Goodbye, #{user_name}!"
-    # end
-
+  get '/goodbye/:name' do    
+    @user_name == params[:name]
+    "Goodbye, #{user_name}!"    
   end
 
   get '/multiply/:num1/:num2' do
