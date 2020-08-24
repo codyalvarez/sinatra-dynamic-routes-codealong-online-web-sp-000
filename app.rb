a191d798cbba3fae902ff513f5e0615db1dad4ce
Cody Alvarez <cody_alvarez19@outlook.com>
2020-08-24 19:35:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ class App < Sinatra::Base
   get '/multiply/:num1/:num2' do
     @multiply = params[:num1/:num2]
       product = num1 / num2
-      binding.pry
+      
       return product
   end
 
